cd415eceeacbf98782739f968452ebfb159e4007
gokal <alishangokal@gmail.com>
2019-05-31 12:30:37 +1200

    initial file upload

---
+++ NoSleep.ps1
@@ -0,0 +1,11 @@
+﻿# This script presses the ScrollLock key every two min, to stop Windows from going to sleep.
+echo "NoSleep is running"
+$wsh = New-Object -ComObject WScript.Shell
+
+
+
+while($true){
+    Start-Sleep -s 50
+    $wsh.SendKeys('{SCROLLLOCK}')
+    echo "NoSleep is still running"
+}
---
+++ TowerWOL.bat
@@ -0,0 +1,2 @@
+cd c:\program files\wolcmd
+wolcmd 7824af37dc9adc9a 192.168.1.173 255.255.255.0
